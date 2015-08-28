c729aa4eaf16b22bc504c9f6dd98e1ca72101247
shunkin <shunkin@sfc.wide.ad.jp>
2015-08-29 08:55:38 +0900

    fixed db scheme

--- db/migrate/20150828124923_add_temples.rb
+++ db/migrate/20150828124923_add_temples.rb
@@ -1,18 +1,21 @@
 class AddTemples < ActiveRecord::Migration
-  def change
-    add_column :temples, :ageLimit,   :integer
-     add_column :temples, :rent,       :integer
-     add_column :temples, :foodExpense,:integer
-     add_column :temples, :minYears,   :integer
-     add_column :temples, :holiday,    :integer
-     add_column :temples, :phoneNumber,:integer
-     add_column :temples, :region_id, :integer
+	def change
+		add_column :temples, :ageLimit,   :integer
+		add_column :temples, :rent,       :integer
+		add_column :temples, :foodExpense,:integer
+		add_column :temples, :minYears,   :integer
+		add_column :temples, :holiday,    :integer
+		add_column :temples, :phoneNumber,:integer
+		add_column :temples, :region_id, :integer
+		add_column :temples, :workingHours, :integer
+		add_column :temples, :distanceFromStation, :integer
 
-     add_column :temples, :picUrl, :string
-     add_column :temples, :costume,:string
-     add_column :temples, :address,:string
+		add_column :temples, :picUrl, :string
+		add_column :temples, :costume,:string
+		add_column :temples, :address,:string
+		add_column :temples, :experience, :string
 
-     add_column :temples, :sponger,:boolean
-     add_column :temples, :wages,  :boolean
-  end
+		add_column :temples, :sponger,:boolean
+		add_column :temples, :wages,  :boolean
+	end
 end
